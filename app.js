4f17be505143464f4cb446e7bcaaa6853a6b4484
NevzanEmreErcan <88339594+NevzanEmreErcan@users.noreply.github.com>
2024-01-02 09:51:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function checkWinner() {
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],    //Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8],    //Columns
-        [0, 4, 8], [2, 4, 6]                //Diognal
+        [0, 4, 8], [2, 4, 6]                //Diagonal
     ]
 
     for (const combination of winningCombinations){
